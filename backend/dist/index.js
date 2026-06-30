884b6244921a07b046c8aec85c5086397c055976
Siddharth Ganvir <siddharthganvir05@gmail.com>
2026-06-30 19:21:30 +0530

    Fix Typescript configuration for Render Deployment

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,7 +12,7 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api/auth", authRoutes_1.default);
 app.use("/api/applications", applicationRoutes_1.default);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
